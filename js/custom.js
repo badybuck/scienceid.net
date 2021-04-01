8a12ba997599cb8a44c536b706aad54900c98438
Pavel <wol1414@gmail.com>
2021-04-01 13:26:02 +0300

    add text

--- js/custom.js
+++ js/custom.js
@@ -11,4 +11,35 @@ window.onload = function () {
 		target.parentElement.classList.toggle("active");
 	})
 
+
+	const tabs = document.querySelector('.sModel');
+
+			tabs.addEventListener('click', function (element) {
+				const btn = element.target.closest(`.diagram-block-item--js:not(.active)`);
+				if (!btn) return;
+				const data = btn.dataset.tab; 
+				const content = this.querySelectorAll(`.toggle-content`);
+				const tabsAllBtn = document.querySelectorAll(".diagram-block-item--js");
+				tabsAllBtn.forEach(element => {
+					element.dataset.tab == data
+						? element.classList.add('active')
+						: element.classList.remove('active')
+				});
+				content.forEach(element => { 
+					// console.log(element.id);
+					element.id == data
+						? (element.classList.add('active'))
+						: element.classList.remove('active')
+				});
+			})
+
+		// $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+		// 	$(this)
+		// 		.addClass('active').siblings().removeClass('active')
+		// 		.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+		// 		.eq($(this).index()).fadeIn().addClass('active');
+
+		// });
+
+
 };
