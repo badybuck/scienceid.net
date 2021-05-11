2652540e4e2969d5ffee26d2d24ddcdaef1deb01
Pavel <wol1414@gmail.com>
2021-05-12 00:23:40 +0300

    add tabs

--- public/js/custom.js
+++ public/js/custom.js
@@ -29,13 +29,32 @@ function eventHandler() {
 				element.id == data ? element.classList.add('active') : element.classList.remove('active');
 			});
 		});
-	} // $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-	// 	$(this)
-	// 		.addClass('active').siblings().removeClass('active')
-	// 		.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-	// 		.eq($(this).index()).fadeIn().addClass('active');
-	// });
+	}
 
+	var tab = 'tabs'; //  if (tabs2) {
+	// 		tabs2.addEventListener('click', function (element) {
+	// 			const btn = element.target.closest(`.tabs__btn:not(.active)`);
+	// 			if (!btn) return;
+	// 			const data = btn.dataset.tab; 
+	// 			const content = this.querySelectorAll(`.tabs__content`);
+	// 			const tabsAllBtn = document.querySelectorAll(".tabs__btn");
+	// 			tabsAllBtn.forEach(element => {
+	// 				element.dataset.tab == data
+	// 					? element.classList.add('active')
+	// 					: element.classList.remove('active')
+	// 			});
+	// 			content.forEach(element => { 
+	// 				// console.log(element.id);
+	// 				element.id == data
+	// 					? (element.classList.add('active'))
+	// 					: element.classList.remove('active')
+	// 			});
+	// 		})
+	// }
+
+	$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+		$(this).addClass('active').siblings().removeClass('active').closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active').eq($(this).index()).fadeIn().addClass('active');
+	});
 }
 
 ;
--- sourse/js/custom.js
+++ sourse/js/custom.js
@@ -40,13 +40,36 @@ function eventHandler() {
 				});
 			})
 	}
-		// $('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
-		// 	$(this)
-		// 		.addClass('active').siblings().removeClass('active')
-		// 		.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
-		// 		.eq($(this).index()).fadeIn().addClass('active');
+	const tab = 'tabs';
+	//  if (tabs2) {
 
-		// });
+
+	// 		tabs2.addEventListener('click', function (element) {
+	// 			const btn = element.target.closest(`.tabs__btn:not(.active)`);
+	// 			if (!btn) return;
+	// 			const data = btn.dataset.tab; 
+	// 			const content = this.querySelectorAll(`.tabs__content`);
+	// 			const tabsAllBtn = document.querySelectorAll(".tabs__btn");
+	// 			tabsAllBtn.forEach(element => {
+	// 				element.dataset.tab == data
+	// 					? element.classList.add('active')
+	// 					: element.classList.remove('active')
+	// 			});
+	// 			content.forEach(element => { 
+	// 				// console.log(element.id);
+	// 				element.id == data
+	// 					? (element.classList.add('active'))
+	// 					: element.classList.remove('active')
+	// 			});
+	// 		})
+	// }
+		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+			$(this)
+				.addClass('active').siblings().removeClass('active')
+				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+				.eq($(this).index()).fadeIn().addClass('active');
+
+		});
 
 };
 	if (document.readyState !== 'loading') {
